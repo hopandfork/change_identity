5abd601a91bfc2b4b0ff52c72eeba3834f87b932
root <root@luca>
2016-12-24 12:37:53 +0100

    Interface name as a fixed parameter and specific config file per interface
    A specific interface name is nedeed to update mac and hostname.
    For each interface the script creates a config file.
    
    Fixes: #2 #1
    Signed-off-by: LucaF89 <luca.fulgieri@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # change_identity
-A script to modify hostname and mac address related to an interface
+A script to modify hostname and mac address related to an interface.
+
+##Prerequisite
+ifconfig and hostname are nedeed.
+
+##Usage
+1) To change the hostname you can use: sudo ./change_identity -n hostname eth0 
+2) To change the mac you can use: sudo ./change_identity -m mac eth0
+3) To change mac and hostname you can use: sudo ./change_identity -n hostname -m mac eth0
+4) To restore mac and hostname you can use the default file "config_eth0": sudo ./change_identity -r eth0
+5) To restore mac and hostname you can use a specific file: sudo ./change_identity -r config_custom eth0
--- change_identity.sh
+++ change_identity.sh
@@ -1,70 +1,88 @@
 parameters_count=$#
-config_file="config"
-hostname=$(hostname)
-if [ "$OSTYPE" = "linux-gnu" ]; then
-	mac=$(ifconfig eth0 | grep HWaddr | cut -d " " -f 11);
-elif [ "$OSTYPE" = "darwin14" ]; then
-	mac=$(ifconfig en0 ether | grep ether | cut -d " " -f 2);
-fi
+interface="${!#}"
+config_file="config_"$interface
 execute=1
 
-#Create a config file
-config_file_exists=0
-file_list=$(ls)
-	for file in $file_list; do \
-		if [ "$file" = "$config_file" ]; then
-			config_file_exists=1;
-		fi
-	done
-
-if [ $config_file_exists -eq 0 ]; then
-	echo "hostname=$hostname" > config 
-	echo "mac=$mac" >> config
-fi
-
 #Parse command line
-if [ $parameters_count -eq 4 ]; then 
-		if [ "$1" = "-n" ] && [ "$3" = "-m" ]; then
-			hostname=$2
-			mac=$4;
-		elif [ "$1" = "-m" ] && [ "$3" = "-n" ]; then
-			hostname=$4
-			mac=$2;
-		fi
-elif [ $parameters_count -eq 2 ]; then 
+if [ $parameters_count -eq 5 ]; then 
+	if [ "$1" = "-n" ] && [ "$3" = "-m" ]; then
+		new_hostname=$2
+		new_mac=$4;
+	elif [ "$1" = "-m" ] && [ "$3" = "-n" ]; then
+		new_hostname=$4
+		new_mac=$2;
+	fi
+elif [ $parameters_count -eq 3 ]; then 
 	if [ "$1" = "-n" ]; then 
-		hostname=$2;
+		new_hostname=$2;
 	elif [ "$1" = "-m" ]; then
-		mac=$2;
+		new_mac=$2;
 	elif [ "$1" = "-r" ]; then
-		hostname=$(grep $2 -e "hostname=" | cut -d "=" -f 2)
-		mac=$(grep $2 -e "mac=" | cut -d "=" -f 2);
-    else
-        echo "Incorrect parameter $1";
+		new_hostname=$(grep $2 -e "hostname=" | cut -d "=" -f 2)
+		new_mac=$(grep $2 -e "mac=" | cut -d "=" -f 2);
+    	else
+        	echo "Incorrect parameter $1"
+		exit 1;
+	fi
+elif [ $parameters_count -eq 2 ]; then
+	if [ "$1" = "-r" ]; then
+		new_hostname=$(grep $config_file -e "hostname=" | cut -d "=" -f 2)
+		new_mac=$(grep $config_file -e "mac=" | cut -d "=" -f 2);
+    	else
+        	echo "Incorrect parameter $1"
+		exit 1;
 	fi
 elif [ $parameters_count -eq 1 ]; then
-    if [ "$1" = "--help" ]; then
-        execute=0
-        echo "-n hostname	Hostname to set, executed twice the settings will
-		be resetted"
-        echo "-m mac		Mac address"
-		echo "-r restore	Restore default value of hostname and mac address,
+    	if [ "$1" = "--help" ]; then
+        	execute=0
+		echo "change_identity [options] interface"
+		echo "-n hostname	Hostname to set"
+        	echo "-m mac		Mac address to set"
+		echo "-r restore	Restore default value of hostname and 
+		mac address es. change_identity -r interface,
 		optionally you can specify a different config file";
-	elif [ "$1" = "-r" ]; then
-		hostname=$(grep $config_file -e "hostname=" | cut -d "=" -f 2)
-		mac=$(grep $config_file -e "mac=" | cut -d "=" -f 2);
-    else
-		execute=0
-        echo "Incorrect parameter $1";
+	else
+        	echo "Incorrect parameter $1"
+		exit 1;
 	fi
 fi
 
-#Execute commands
 if [ $execute -eq 1 ]; then
-	hostname "$hostname"
+	#[BUG 1] ifconfig problem: eth0 -> mac_eth0 and eth -> mac_eth0 
+	hostname=$(hostname)
 	if [ "$OSTYPE" = "linux-gnu" ]; then
-		ifconfig eth0 hw ether "$mac";
+		mac=$(ifconfig $interface | grep HWaddr | cut -d " " -f 11);
 	elif [ "$OSTYPE" = "darwin14" ]; then
-		ifconfig en0 ether "$mac";
+		mac=$(ifconfig $interface ether | grep ether | cut -d " " -f 2);
+	fi
+	
+	#Create a config file
+	config_file_exists=0
+	file_list=$(ls)
+		for file in $file_list; do \
+			if [ "$file" = "$config_file" ]; then
+				config_file_exists=1;
+			fi
+		done
+	
+	if [ $config_file_exists -eq 0 ]; then
+		echo "hostname=$hostname" > "$config_file" 
+		echo "mac=$mac" >> "$config_file"
+	fi
+	
+	#Execute commands
+	if [ "$new_hostname" = "" ]; then
+		new_hostname=$hostname;
+	fi
+	
+	if [ "$new_mac" = "" ]; then
+		new_mac=$mac;
+	fi
+	
+	hostname "$new_hostname"
+	if [ "$OSTYPE" = "linux-gnu" ]; then
+		ifconfig eth0 hw ether "$new_mac";
+	elif [ "$OSTYPE" = "darwin14" ]; then
+		ifconfig en0 ether "$new_mac";
 	fi
 fi
